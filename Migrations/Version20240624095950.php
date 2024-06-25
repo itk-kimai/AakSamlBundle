6b0df71f1fd3c4701ca97e2b7fbbdf52e4aac7ed
turegjorup <ture@ignatz.dk>
2024-06-25 11:14:26 +0200

    1721: Fix namespace typo

--- Migrations/Version20240624095950.php
+++ Migrations/Version20240624095950.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace KimaiPlugin2\AakSamlBundle\Migrations;
+namespace KimaiPlugin\AakSamlBundle\Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
